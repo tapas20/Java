a6550089b4f2a5586f5645b200f1572d984a8b85
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-05-12 23:37:36 +0530

    printInRange

--- BST.java
+++ BST.java
@@ -105,6 +105,22 @@ public class BST {
         path.remove(path.size() - 1);
     }
 
+    public static void printInRange(Node root, int k1, int k2) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.data >= k1 && root.data <= k2) {
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        } else if (root.data < k1) {
+            printInRange(root.right, k1, k2);
+        } else if (root.data > k2) {
+            printInRange(root.left, k1, k2);
+        }
+    }
+
     public static void main(String[] args) {
         int[] values = { 8, 5, 3, 6, 10, 11, 14 };
         Node root = null;
@@ -122,6 +138,8 @@ public class BST {
 
         // inOrder(newTree);
 
-        printRoot2Leaf(root, new ArrayList<>());
+        // printRoot2Leaf(root, new ArrayList<>());
+
+        printInRange(root, 5, 12);
     }
 }
